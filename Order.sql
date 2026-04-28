df47f5954b1e61af46db68a1a3abbd121511d772
Renz <dogetheink@gmail.com>
2026-04-28 22:28:37 +0800

    Create Order.sql

---
+++ Order.sql
@@ -0,0 +1,10 @@
+-- SORTS THE HIGHEST TO LOWEST SCORE
+SELECT * FROM players
+ORDER BY score DESC
+LIMIT 6;
+
+-- Show players with score 150 and above, sorted from highest to lowest
+SELECT name, score
+FROM players
+WHERE score >= 150
+ORDER BY score DESC
